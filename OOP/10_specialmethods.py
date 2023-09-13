724cb91d63fc6bc89a3b98642b930c22448c9b85
RyanNgCT <48358569+RyanNgCT@users.noreply.github.com>
2023-09-13 15:46:30 +0800

    dunder methods

--- OOP/10_specialmethods.py
+++ OOP/10_specialmethods.py
@@ -16,11 +16,24 @@ class Employee:
     def applyRaise(self):
         self.pay *= Employee.raiseAmount
 
+    # unambiguous representation of object -> meant for other devs to see
     def __repr__(self):
-        pass
+        return f"Employee('{self.firstName}', '{self.lastName}', {self.pay})"
 
+    # to-string method
     def __str__(self):
-        pass
+        return f'{self.printFullName()} - {self.email}'
+    
+    # adds 2 employee's salaries together, other is next object to reference
+    def __add__(self, other):
+        return self.pay + other.pay
+
 
 emp1 = Employee(1, "Hohn", "Jammy", 500)
 emp2 = Employee(2, "Moike", "Wasowski", 150)
+
+print(repr(emp1), repr(emp2))
+print(str(emp1))
+
+print(int.__add__(5, 3)) # using in-built function for type of int
+print(emp1 + emp2)
